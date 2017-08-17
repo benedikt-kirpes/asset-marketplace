fa41c02b7d2343a8efe8b4a3a32fffedcd5d7d51
lasse <orda@elektro.dtu.dk>
2017-08-17 13:58:56 +0200

    added javascript back, yaay

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,5 +1,5 @@
 // Import the page's CSS. Webpack will know what to do with it.
-import "../stylesheets/app.css";
+//import "../stylesheets/app.css";
 
 // Import libraries we need.
 import { default as Web3} from 'web3';
@@ -29,8 +29,8 @@ window.App = {
       try {
           MetaCoin.setProvider(web3.currentProvider);
       } catch (err) {
-          document.getElementById("signupbox").style.display = "none";
-          document.getElementById("notloggedin").style.display = "inline";
+          // document.getElementById("signupbox").style.display = "none";
+          // document.getElementById("notloggedin").style.display = "inline";
           return;
       }
     PropertyOwners.setProvider(web3.currentProvider);
@@ -38,15 +38,15 @@ window.App = {
     // Get the initial account balance so it can be displayed.
     web3.eth.getAccounts(function(err, accs) {
       if (err != null) {
-        document.getElementById("signupbox").style.display = "none";
-        document.getElementById("notloggedin").style.display = "inline";
+        // document.getElementById("signupbox").style.display = "none";
+        // document.getElementById("notloggedin").style.display = "inline";
           alert("There was an error fetching your accounts.");
         return;
       }
 
       if (accs.length == 0) {
-          document.getElementById("signupbox").style.display = "none";
-          document.getElementById("notloggedin").style.display = "inline";
+          // document.getElementById("signupbox").style.display = "none";
+          // document.getElementById("notloggedin").style.display = "inline";
         alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
         return;
       }
@@ -165,18 +165,18 @@ window.App = {
   }
 };
 
-window.addEventListener('load', function() {
-  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-  if (typeof web3 !== 'undefined') {
-      console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
-      // Use Mist/MetaMask's provider
-      window.web3 = new Web3(web3.currentProvider);
-      console.log("PROVIDER; ")
-      console.log(web3.currentProvider)
-  } else {
-    console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    //window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-  }
-  App.start();
-});
+// window.addEventListener('load', function() {
+//   // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+//   if (typeof web3 !== 'undefined') {
+//       console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
+//       // Use Mist/MetaMask's provider
+//       window.web3 = new Web3(web3.currentProvider);
+//       console.log("PROVIDER; ")
+//       console.log(web3.currentProvider)
+//   } else {
+//     console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
+//     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+//     //window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+//   }
+//   App.start();
+// });
