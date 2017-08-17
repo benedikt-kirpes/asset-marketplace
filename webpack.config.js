d96cf941a37300197f8bffbdb7fa1f8bad4cce28
Benedikt K <benkr@web.de>
2017-08-17 10:44:29 +0200

    added seller and buyer

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,8 @@ module.exports = {
         { from: './app/vendor', to: "./vendor" },
         { from: './app/js', to: "./js" },
         { from: './app/index.html', to: "." },
+        { from: './app/buyer.html', to: "." },
+        { from: './app/seller.html', to: "." },
         { from: './app/log.html', to: "." }
     ])
   ],
